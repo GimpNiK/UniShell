3d2f40cbb70258d5e14d22070c86e140f551ab9f
ItisInow <reshfizika@mail.ru>
2025-12-23 21:15:21 +0300

    Fix bug import in unishell_win

--- unishell_win/unishell_win/regedit/__init__.py
+++ unishell_win/unishell_win/regedit/__init__.py
@@ -1,7 +1,7 @@
 from .UserType import CurrentUser, User, Users
-from PATH import PATH as stdPATH
-from AutoRun import AutoRun as stdAutoRun
-from AutoRunOnce import AutoRunOnce as stdAutoRunOnce
+from .PATH import PATH as stdPATH
+from .AutoRun import AutoRun as stdAutoRun
+from .AutoRunOnce import AutoRunOnce as stdAutoRunOnce
 
 CurUser = CurrentUser()
 PATH = stdPATH(CurUser)
